bd1372571f7834282e4db2395dcffd62b4155675
wendeI1 <wendelhenrique1311@gmail.com>
2026-01-16 11:46:53 -0300

    Arrays pt.01

--- src/academy/devdojo/maratonajava/introducao/Aula07Array01.java
+++ src/academy/devdojo/maratonajava/introducao/Aula07Array01.java
@@ -3,5 +3,14 @@ package academy.devdojo.maratonajava.introducao;
 public class Aula07Array01 {
     public static void main(String[] args) {
 
+        int[] idades = new int[10];
+
+        for (int i = 1; i < idades.length; i ++){
+            idades[i] = i;
+        }
+
+        idades[9] = 5;
+
+        System.out.println(idades[9]);
     }
 }
